eab84993507db4fd7c0fc6279363a74c84ac935f
pardha saradhi <pardha18145@gmail.com>
2024-07-23 17:14:15 -0400

    token authentication is implemented

--- main/models.py
+++ main/models.py
@@ -16,7 +16,7 @@ class ProductCategory(models.Model):
         return self.title
 
 class Product(models.Model):
-    category=models.ForeignKey(ProductCategory,on_delete=models.SET_NULL,null=True)
+    category=models.ForeignKey(ProductCategory,on_delete=models.SET_NULL,null=True,related_name='category_product')
     vendor=models.ForeignKey(Vendor,on_delete=models.SET_NULL,null=True)
     title=models.CharField(max_length=200)
     detail=models.TextField(null=True)
@@ -24,3 +24,27 @@ class Product(models.Model):
 
     def _str_(self):
         return self.title
+    
+#Customer model
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile=models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+#Order model
+class Order(models.Model):
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_time=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.order_time
+
+#Order item model
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, on_delete=models.CASCADE,related_name='order_items')
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.pk
--- main/serializers.py
+++ main/serializers.py
@@ -7,7 +7,7 @@ class VendorSerializer(serializers.ModelSerializer):
         fields=['id','user','address']
     def __init__(self, *args, **kwargs):
         super(VendorSerializer, self).__init__( *args, **kwargs)
-        self.Meta.depth=1
+        # self.Meta.depth=1
 
 class VendorDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,7 +15,7 @@ class VendorDetailSerializer(serializers.ModelSerializer):
         fields=['id','user','address']
     def __init__(self, *args, **kwargs):
         super(VendorDetailSerializer, self).__init__( *args, **kwargs)
-        self.Meta.depth=1
+        # self.Meta.depth=1
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,7 +23,7 @@ class ProductListSerializer(serializers.ModelSerializer):
         fields=['id','category','vendor','title','detail','price']
     def __init__(self, *args, **kwargs):
         super(ProductListSerializer, self).__init__( *args, **kwargs)
-        self.Meta.depth=1
+        # self.Meta.depth=1
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,4 +31,38 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         fields=['id','category','vendor','title','detail','price']
     def __init__(self, *args, **kwargs):
         super(ProductDetailSerializer, self).__init__( *args, **kwargs)
-        self.Meta.depth=1
+        # self.Meta.depth=1
+
+#customer serializer
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Customer
+        fields=['id','user','mobile']
+    def __init__(self, *args, **kwargs):
+        super(CustomerSerializer, self).__init__( *args, **kwargs)
+        # self.Meta.depth=1
+
+class CustomerDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Customer
+        fields=['id','user','mobile']
+    def __init__(self, *args, **kwargs):
+        super(CustomerDetailSerializer, self).__init__( *args, **kwargs)
+        # self.Meta.depth=1
+
+#order serializer
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Order
+        fields=['customer','order_time']
+    def __init__(self, *args, **kwargs):
+        super(OrderSerializer, self).__init__( *args, **kwargs)
+        # self.Meta.depth=1
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Order
+        fields=['id', 'customer', 'order_time', 'order_items']
+    def __init__(self, *args, **kwargs):
+        super(OrderDetailSerializer, self).__init__( *args, **kwargs)
+        # self.Meta.depth=1
